a9d633f967e83912d5001b9d94a95cc7ea495790
Praskovya Akhmetova <praskovya.akhmetova.93@mail.ru>
2023-04-03 23:33:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int pricePerMile = 20;
+        int oneMileBonus = 1;
+        int accrucedBonuses = ticketPrice / pricePerMile * oneMileBonus;
+
+        System.out.println(accrucedBonuses = ticketPrice / pricePerMile * oneMileBonus);
+    }
+}
